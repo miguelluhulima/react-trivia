2e4b4b60d5f9a996be6590f5a5293e6c38fe57e2
miguelluhulima <103628494+miguelluhulima@users.noreply.github.com>
2022-12-29 16:55:56 +0700

    enable start menu toggle to render quiz

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import Quiz from "./components/Quiz"
 import Start from "./components/Start"
 
 export default function App() {
+    const [start, setStart] = useState(false)
+
+    function toggle() {
+        setStart(prevState => !prevState)
+    }
+
     return (
         <main>
-            {/* <Start /> */}
-            <Quiz />
+            {start === false ? <Start handleClick={toggle} /> : <Quiz />}
         </main>
     )
 }
--- src/components/Start.js
+++ src/components/Start.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-export default function Start() {
+export default function Start(props) {
     return (
         <div className="start">
             <h1 className="start--title">Quizzical</h1>
             <p className="start--text">A trivia game, made in React</p>
-            <button className="start--button">Start Quiz</button>
+            <button className="start--button" onClick={props.handleClick}>Start Quiz</button>
         </div>
     )
 }
